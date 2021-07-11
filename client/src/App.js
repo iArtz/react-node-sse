ae80e1e4ee5df2f5328ae41e3cd3347e42df97c5
iArtz <kanitphong@uservices-thailand.com>
2021-07-11 11:55:12 +0700

    :tada: frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [process, setProcess] = useState({})
+  const [message, setMessage] = useState({})
+  const [listening, setListening] = useState(false)
+
+  const statusMessage = {
+    subscribed: 'Subscribed',
+    unsubscribed: 'Unsubscribed',
+  }
+
+  const subscribe = async () => {
+    const status = listening
+    if (!status) {
+      const events = new EventSource('http://localhost:8000/events')
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data)
+        console.log(event)
+        switch (parsedData.type) {
+          case 'init-connection':
+            setProcess(parsedData.processId)
+            break
+          case 'message':
+            setMessage(parsedData.message)
+            break
+          default:
+            return
+        }
+      }
+    } else {
+      setProcess({})
+      setMessage({})
+      await axios.delete(`http://localhost:8000/closes/${process}`)
+    }
+    setListening(!listening)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{listening ? statusMessage.subscribed : statusMessage.unsubscribed}</p>
+      <p>{JSON.stringify(process)}</p>
+      <button onClick={subscribe}>
+        {listening ? statusMessage.unsubscribed : statusMessage.subscribed}
+      </button>
+      <br />
+      <p>{JSON.stringify(message)}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,10 @@
 import { app } from './app'
 
 const start = async () => {
-
-const PORT = process.env.PORT || 3000;
-	app.listen(PORT, () => {
-		console.log(`Listening on port ${PORT}!!!`)
-	})
+  const PORT = process.env.PORT || 8000
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!!!`)
+  })
 }
 
 start()
